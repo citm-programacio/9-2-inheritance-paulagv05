63d0c088638638b598edebb38be8a00ff6b5ff24
paulagv05 <paula.garcia.valls@estudiantat.upc.edu>
2024-11-14 17:06:25 +0100

    Update ConsoleApplication19.cpp

--- ConsoleApplication19/ConsoleApplication19.cpp
+++ ConsoleApplication19/ConsoleApplication19.cpp
@@ -1,27 +1,71 @@
 #include <iostream>
 using namespace std;
 
-class building {
+class Building {
 protected:
     string name;
 public:
-    building(string _n) {
-        name = _n;
-    }
+    Building(string _n) : name(_n) {}
     string getName() {
         return name;
     }
 };
 
-class warehouse : public building {
+class Warehouse : public Building {
+private:
+    int wood;
+    int rocks;
+    int wheat;
 
+public:
+    Warehouse(string _n, int _w, int _r, int _wh)
+        : Building(_n), wood(_w), rocks(_r), wheat(_wh) {}
 
+    void printResources() {
+        cout << "Name: " << name << endl;
+        cout << "Wood: " << wood << ", Rocks: " << rocks << ", Wheat: " << wheat << endl;
+    }
 };
 
-int main()
-{
-    string depName = 'deposit';
-    warehouse warez(depName);
-    cout <<
+class House : public Building {
+private:
+    int floors;
+    int inhabitants;
+    int servants;
 
+public:
+    House(string _n, int _f, int _i, int _s)
+        : Building(_n), floors(_f), inhabitants(_i), servants(_s) {}
+
+    void printHouse() {
+        cout << endl << "Name: " << name << endl;
+        cout << "Floors: " << floors << ", Inhabitants: " << inhabitants << ", Servants: " << servants << endl;
+    }
+};
+
+class Temple : public Building {
+private:
+    string god;
+    int priests;
+
+public:
+    Temple(string _n, string _g, int _p)
+        : Building(_n), god(_g), priests(_p) {}
+
+    void printTemple() {
+        cout << endl << "Name: " << name << endl;
+        cout << "God: " << god << ", Priests: " << priests << endl;
+    }
+};
+
+int main() {
+    Warehouse warez("Warehouse", 120, 21, 12);
+    House house("Roman House", 2, 5, 3);
+    Temple temple("Temple of Jupiter", "Jupiter", 10);
+
+    warez.printResources();
+    house.printHouse();
+    temple.printTemple();
+
+    return 0;
 }
